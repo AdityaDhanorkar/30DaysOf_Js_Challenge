fa055f0dc83c68e16dbbdcf9165a73bc0f76160a
Aditya Parshram Dhanorkar <118669062+AdityaDhanorkar@users.noreply.github.com>
2024-07-13 11:46:31 +0000

    Fisrt Day

---
+++ Day_01/01_Task.js
@@ -0,0 +1,54 @@
+//Variables
+var number = 261;
+console.log(number)
+
+let name = 'Aditya'
+console.log(name);
+
+const Boolean = true;
+console.log(Boolean)
+
+//Data Type
+
+let Name = 'Aditya';
+let age = 22;
+let yesOrno=true;
+let city=null;
+let state
+let sub = ['math','marathi']
+
+const object1 ={
+    'Degree':'BE',
+    'PassOutYear':'2024'
+}
+
+
+console.log(typeof(Name))
+console.log(Name)
+console.log(typeof(age))
+console.log(age)
+console.log(typeof(yesOrno))
+console.log(yesOrno)
+console.log(typeof(city))
+console.log(city)
+console.log(typeof(state))
+console.log(state)
+console.log(typeof(sub))
+console.log(sub)
+console.log(typeof(object1))
+console.log(object1)
+
+
+
+
+//Difference BetWeen Let and Const
+let first=12;
+console.log(first)
+first = 24;
+console.log(first)
+
+
+const sec=25;
+console.log(sec);
+// sec=50; //TypeError: Assignment to constant variable.
+console.log(sec) 
